4a21e33a1e0717ae2cc782f5c4965c755d774608
Tim <timothymcfadden@scom2k7.com>
2015-04-21 15:28:16 -0400

    Test

---
+++ SQLBasedGroupDemo/Team1GroupDiscovery.vbs
@@ -0,0 +1,39 @@
+﻿ Dim SourceId
+    Dim objConnection
+    Dim oRS
+    Dim sConnectString
+    Dim ManagedEntityID
+    Dim oAPI
+    Dim oDiscoveryData
+
+    SourceId                = WScript.Arguments(0)
+    ManagedEntityId         = WScript.Arguments(1)
+
+    Set oAPI                = CreateObject("MOM.ScriptAPI")
+    Set oDiscoveryData      = oAPI.CreateDiscoveryData(0,SourceId,ManagedEntityId)
+
+    sConnectString = "Driver={SQL Server}; Server=xDB01; Database=CMDB;"
+
+    Set objConnection = CreateObject("ADODB.Connection")
+    objConnection.Open sConnectString
+
+    Set oRS = CreateObject("ADODB.Recordset")
+    oRS.Open "select ServerName from ServerList where MonitorGroup = 'Team 1'", objConnection
+
+    Set groupInstance = oDiscoveryData.CreateClassInstance("$MPElement[Name='SQLBasedGroupDemo.Team1Group']$")
+
+    While Not oRS.EOF
+
+    Set serverInstance = oDiscoveryData.CreateClassInstance("$MPElement[Name='Windows!Microsoft.Windows.Computer']$")
+    serverInstance.AddProperty "$MPElement[Name='Windows!Microsoft.Windows.Computer']/PrincipalName$",oRS.Fields("ServerName")
+    Set relationshipInstance = oDiscoveryData.CreateRelationshipInstance("$MPElement[Name='SQLBasedGroupDemo.Team1GroupContainsWindowsComputers']$")
+    relationshipInstance.Source = groupInstance
+    relationshipInstance.Target = serverInstance
+    oDiscoveryData.AddInstance relationshipInstance
+    oRS.MoveNext
+
+    Wend
+
+    objConnection.Close
+
+    Call oAPI.Return(oDiscoveryData)
---
+++ SQLBasedGroupDemo/Team2GroupDiscovery.ps1
@@ -0,0 +1,57 @@
+﻿param($sourceId,$managedEntityId)
+
+#Start by setting up API object and creating a discovery data object.
+#Discovery data object requires the MPElement and Target/ID variables.  The first argument in the method is always 0.
+$api = New-Object -comObject 'MOM.ScriptAPI'
+$discoveryData = $api.CreateDiscoveryData(0, $sourceId, $managedEntityId)
+
+Import-Module -Name "OperationsManager" 
+New-SCManagementGroupConnection -ComputerName:"localhost";
+
+$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = "Server = xDB01; Database = CMDB; Integrated Security = True"
+
+$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+$SqlCmd.CommandText = "select ServerName from ServerList where MonitorGroup = 'Team 3'"
+$SqlCmd.Connection = $SqlConnection
+
+$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+$SqlAdapter.SelectCommand = $SqlCmd
+
+$myDataSet = New-Object System.Data.DataSet
+$SqlAdapter.Fill($myDataSet)
+
+$SqlConnection.Close()
+
+$Servers = $myDataSet.Tables[0]
+
+#Get the top level folder and then enumerate through each subfolder.
+
+$groupInstance = $discoveryData.CreateClassInstance("$MPElement[Name='SQLBasedGroupDemo.Team2Group']$")
+
+#$myArray = "xSP01.scom2k12.com","xDV01.scom2k12.com"
+
+foreach ($element in $myDataSet.Tables[0].Rows.ServerName)
+{
+	$class_wincomputer = Get-SCOMClass -name System.Computer 
+	$mmcomputerobject = get-scommonitoringobject -Class $class_wincomputer | where{$_.displayname -eq $element} 
+
+		if($mmcomputerobject) 
+		{ 
+			$serverInstance = $discoveryData.CreateClassInstance("$MPElement[Name='Windows!Microsoft.Windows.Computer']$")
+			$serverInstance.AddProperty("$MPElement[Name='Windows!Microsoft.Windows.Computer']/PrincipalName$", $element)
+
+			$RelationshipInstance = $discoveryData.CreateRelationshipInstance("$MPElement[Name='SQLBasedGroupDemo.Team2GroupContainsWindowsComputers']$")
+			$RelationshipInstance.Source = $groupInstance
+			$RelationshipInstance.Target = $serverInstance
+			$discoveryData.AddInstance($RelationshipInstance)
+		}
+		elseif(!$mmcomputerobject)
+		{
+			Write-Eventlog -Logname 'Windows PowerShell' -Source PowerShell -EventID 600 -EntryType Error -Message "$element Not Found"
+		}
+}
+
+
+#Return the discovery data.
+$discoveryData
